58a67ed102dd84e9d8c9daa4f16571ca0f586840
Jonathan Fernandes <int3rlop3r@yahoo.in>
2022-06-14 17:52:13 +0530

    use setjump to jump, parser correct values from opcodes

--- chip.go
+++ chip.go
@@ -23,12 +23,30 @@ type Chip struct {
 	sound    uint8
 	keyboard [16]byte
 	display  [32][63]byte
+
+	jmp bool // this flag is not part of the spec, used for iteration
 }
 
 func NewChip() *Chip {
 	var chip Chip
 	chip.pc = startOff
 	return &chip
+}
+
+func (c *Chip) NextInstr() {
+	// if the `pc` was manually set
+	// don't increment by 2
+	if !c.jmp {
+		c.pc += 2
+	} else {
+		// reset value for next iteration
+		c.jmp = !c.jmp
+	}
+}
+
+func (c *Chip) SetJump(val uint16) {
+	c.jmp = true
+	c.pc = val
 }
 
 func (c *Chip) LoadROM(path string) error {
@@ -61,12 +79,19 @@ func (c *Chip) LoadROM(path string) error {
 
 func (c *Chip) Run() error {
 	var opcode uint16
-	//for ; c.pc < memSize-1; c.pc += 2 {
-	for i := 0; i < 10; i++ { // for now lets just disassemble 10 opcodes
+	var cntr int
+	for ; c.pc < memSize-1; c.NextInstr() {
+		// for now lets just deal with 10 instructions
+		if cntr == 10 {
+			break
+		} else {
+			cntr++
+		}
+
 		opcode = uint16(c.mem[c.pc])<<8 | uint16(c.mem[c.pc+1])
 		fmt.Printf("pc:%02x, mem:%02x, shift:%02x, opcode: %04x\n",
 			c.pc, c.mem[c.pc], uint16(c.mem[c.pc])<<8, opcode)
-		//c.pc += 2
+
 		switch opcode & 0xF000 {
 		case 0x0000:
 			switch opcode & 0x00FF {
@@ -77,48 +102,59 @@ func (c *Chip) Run() error {
 			default:
 				return fmt.Errorf("unknown operation: %04x", opcode)
 			}
-			c.pc += 2
 		case 0x1000: // jmp addr
 			c.pc = opcode & 0x0FFF
 		case 0x2000: // call addr
 			c.stack[c.sp] = c.pc
 			c.sp++
-			c.pc = opcode & 0x0FFF
+			c.SetJump(opcode & 0x0FFF)
 		case 0x3000: // SE Vx, byte
-			x := opcode & 0x0F00
+			x := (opcode & 0x0F00) >> 8
 			kk := byte(opcode & 0x00FF)
 			if c.v[x] == kk { // @TODO: check if x == 'F' ?
 				c.pc += 2
 			}
-			c.pc += 2
 		case 0x4000: // SNE Vx, byte
-			x := opcode & 0x0F00
+			x := (opcode & 0x0F00) >> 8
 			kk := byte(opcode & 0x00FF)
 			if c.v[x] != kk { // @TODO: check if x == 'F' ?
 				c.pc += 2
 			}
-			c.pc += 2
 		case 0x5000: // SE Vx, Vy
-			x := opcode & 0x0F00
-			y := opcode & 0x00F0
+			x := (opcode & 0x0F00) >> 8
+			y := (opcode & 0x00F0) >> 4
 			if c.v[x] == c.v[y] { // @TODO: check if x == 'F' ?
 				c.pc += 2
 			}
-			c.pc += 2
 		case 0x6000: // LD Vx, byte
-			x := opcode & 0x0F00
+			x := (opcode & 0x0F00) >> 8
 			kk := byte(opcode & 0x00FF)
 			c.v[x] = kk
-			c.pc += 2
 		case 0x7000: // Add Vx, byte
-			x := opcode & 0x0F00
+			x := (opcode & 0x0F00) >> 8
 			kk := byte(opcode & 0x00FF)
 			c.v[x] += kk
-			c.pc += 2
 		case 0x8000: // LD Vx, Vy
-			// @WIP
-			x := opcode & 0x0F00
-			y := opcode & 0x00F0
+			x := (opcode & 0x0F00) >> 8
+			y := (opcode & 0x00F0) >> 4
+			switch opcode & 0x000F {
+			case 0x0000:
+				c.v[x] = c.v[y]
+			case 0x0001:
+				c.v[x] |= c.v[y]
+			case 0x0002:
+				c.v[x] &= c.v[y]
+			case 0x0003:
+				c.v[x] ^= c.v[y]
+			case 0x0004:
+				c.v[x] += c.v[y]
+			case 0x0001:
+			case 0x0001:
+			case 0x0001:
+			case 0x0001:
+			default:
+				return fmt.Errorf("unknown operation: %04x", opcode)
+			}
 			if c.v[x] == c.v[y] { // @TODO: check if x == 'F' ?
 				c.pc += 2
 			}
@@ -129,4 +165,5 @@ func (c *Chip) Run() error {
 			return fmt.Errorf("unknown operation: %04x", opcode)
 		}
 	}
+	return nil
 }
