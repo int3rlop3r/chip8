4b736640931f8fd6bd9b865de21ee3663f333cac
Jonathan Fernandes <int3rlop3r@yahoo.in>
2022-06-14 13:47:18 +0530

    implemented 5 opcodes so far

--- chip.go
+++ chip.go
@@ -16,10 +16,19 @@ type Chip struct {
 	mem      [memSize]byte
 	v        [16]byte
 	i        uint16
+	pc       uint16
+	sp       uint16
+	stack    [16]uint16
 	delay    uint8
 	sound    uint8
 	keyboard [16]byte
 	display  [32][63]byte
+}
+
+func NewChip() *Chip {
+	var chip Chip
+	chip.pc = startOff
+	return &chip
 }
 
 func (c *Chip) LoadROM(path string) error {
@@ -48,4 +57,58 @@ func (c *Chip) LoadROM(path string) error {
 	}
 
 	return nil
+}
+
+func (c *Chip) Run() error {
+	var opcode uint16
+	//for ; c.pc < memSize-1; c.pc += 2 {
+	for i := 0; i < 10; i++ { // for now lets just disassemble 10 opcodes
+		opcode = uint16(c.mem[c.pc])<<8 | uint16(c.mem[c.pc+1])
+		fmt.Printf("pc:%02x, mem:%02x, shift:%02x, opcode: %04x\n",
+			c.pc, c.mem[c.pc], uint16(c.mem[c.pc])<<8, opcode)
+		//c.pc += 2
+		switch opcode & 0xF000 {
+		case 0x0000:
+			switch opcode & 0x00FF {
+			case 0x00E0:
+				fmt.Println("CLS")
+			case 0x00EE:
+				fmt.Println("RET")
+			default:
+				return fmt.Errorf("unknown operation: %04x", opcode)
+			}
+			c.pc += 2
+		case 0x1000: // jmp addr
+			c.pc = opcode & 0x0FFF
+		case 0x2000: // call addr
+			c.stack[c.sp] = c.pc
+			c.sp++
+			c.pc = opcode & 0x0FFF
+		case 0x3000: // SE Vx, byte
+			x := opcode & 0x0F00
+			kk := byte(opcode & 0x00FF)
+			if c.v[x] == kk { // @TODO: check if x == 'F' ?
+				c.pc += 2
+			}
+			c.pc += 2
+		case 0x4000: // SNE Vx, byte
+			x := opcode & 0x0F00
+			kk := byte(opcode & 0x00FF)
+			if c.v[x] != kk { // @TODO: check if x == 'F' ?
+				c.pc += 2
+			}
+			c.pc += 2
+		case 0x5000: // SE Vx, Vy
+			x := opcode & 0x0F00
+			y := opcode & 0x00F0
+			if c.v[x] == c.v[y] { // @TODO: check if x == 'F' ?
+				c.pc += 2
+			}
+			c.pc += 2
+		case 0xA000:
+			c.i = opcode & 0x0FFF
+		default:
+			return fmt.Errorf("unknown operation: %04x", opcode)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -9,11 +9,13 @@ func main() {
 	romPath := flag.String("rom", "", "path to a valid CHIP-8 ROM")
 	flag.Parse()
 
-	var chip Chip
+	chip := NewChip()
 	err := chip.LoadROM(*romPath)
 	if err != nil {
 		fmt.Println("err:", err)
 		return
 	}
 	fmt.Println("chip-8 emulator!", len(chip.mem))
+	fmt.Println("testing")
+	chip.Run()
 }
